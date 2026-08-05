10330a2851ce3fb28321a842fa08e0e0da56a465
PiuiP <alinaminok@gmail.com>
2026-08-05 19:15:21 +0300

    add: abstract interface for Transaction

---
+++ app/repositories/transaction.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+from app.models.transaction import TransactionCreate, TransactionResponse
+
+class ITransactionRepository(ABC):
+    
+    @abstractmethod
+    async def create(self, model: TransactionCreate) -> TransactionResponse:
+        pass 
+
+    @abstractmethod
+    async def get_by_id(self, id: UUID) -> TransactionResponse:
+        pass
+
+    @abstractmethod
+    async def get_all(self) -> list[TransactionResponse]:
+        pass
+    
+    @abstractmethod
+    async def update(self, id: UUID, model: TransactionCreate) -> TransactionResponse:
+        pass
+
+    @abstractmethod
+    async def delete(self, id: UUID) -> bool:
+        pass
+
+    # методы для связи с категориями
+    
+    @abstractmethod
+    async def add_categories(self, transaction_id: UUID, category_ids: list[UUID]) -> None:
+        pass
+
+    @abstractmethod
+    async def get_categories_by_transaction_id(self, transaction_id: UUID) -> list[UUID]:
+        pass
